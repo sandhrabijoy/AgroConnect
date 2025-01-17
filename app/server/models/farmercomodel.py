422413a3004e5ce8a3a067d7418182dc0b1462c5
sandhra <sandhrasonabijoy@gmail.com>
2025-01-17 14:13:33 +0900

    created the initial structure for farmer company model

--- app/server/models/farmercomodel.py
+++ app/server/models/farmercomodel.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine,Column,Integer,String, Boolean, DateTime
 
 Base=declarative_base()
 
-#ORM model
+#ORM model for farmer company
 class Farmer(Base):
     __tablename__:'farmercompany'
     id = Column(Integer, primary_key=True, index=True)
@@ -19,3 +19,41 @@ class Farmer(Base):
     updated_by = Column(String, nullable=False)
 
 
+# Creation of initial farmer structure
+class FarmerSchema(BaseModel):
+    name: str
+    code:int
+    active:bool
+    created_at: date
+    created_by: str
+    updated_at: date
+    updated_by: str
+
+    # Adding schema example
+    class Config:
+        orm_mode = True
+
+# Update schema for PUT operation
+class UpdatedFarmerSchema(BaseModel):
+    name: Optional[str]
+    code=Optional[int]
+    active=Optional[bool]
+    created_at: Optional[date]
+    created_by: Optional[str]
+    updated_at: Optional[date]
+    updated_by: Optional[str]
+
+    # Adding schema example
+    class Config:
+        orm_mode = True
+        
+#define the response messaage to be obtained
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+#Define the error message to be obtained
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
