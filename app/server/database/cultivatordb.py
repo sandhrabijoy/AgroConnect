4e596f92f9e48b203fbb8d8afe6dd67d6db7dd81
sandhra <sandhrasonabijoy@gmail.com>
2025-01-17 16:28:30 +0900

    cultivator database and route creation

---
+++ app/server/database/cultivatordb.py
@@ -0,0 +1,52 @@
+from app.server.models.cultivatormodel import Cultivator
+from sqlalchemy.orm import Session
+
+def cultivator_company_helper(CultivatorData:Cultivator) -> dict:
+    return {
+        "id":CultivatorData.id,
+        "name":CultivatorData.name,
+        "code":CultivatorData.code,
+        "active":CultivatorData.active,
+        "created_at": CultivatorData.created_at,
+        "created_by": CultivatorData.created_by,
+        "updated_at": CultivatorData.updated_at,
+        "updated_by":  CultivatorData.updated_by,
+    }
+
+def getCultivatorData(db:Session) ->Cultivator:
+    CultivatorData = db.query(Cultivator).all()
+    return CultivatorData
+
+def addCultivatorData(companyData:dict,db:Session)->Cultivator:
+    try:
+        new_data = Cultivator(**companyData)   
+        db.add(new_data)
+        db.commit()
+        db.refresh(new_data)
+        return new_data
+    except:
+        return "Unable to add the data to the database"
+    
+def update_cultivator_company(db:Session,id:int,data:dict,)->bool:
+        existing_cultivator_company = db.query(Cultivator).filter(Cultivator.id == id).first()
+        if not existing_cultivator_company:
+             return False
+        for key, value in data.items():
+             setattr(existing_cultivator_company,key,value)
+        db.commit()
+        db.refresh(existing_cultivator_company)
+        return True             
+
+def get_id_cultivator_company(db:Session,id:int):
+     cultivator_company = db.query(Cultivator).filter(Cultivator.id == id).first()
+     if cultivator_company:
+        return cultivator_company
+     return None
+
+def delete_company(id:int,db:Session)->bool:
+     existing_company = db.query(Cultivator).filter(Cultivator.id == id).first()
+     if existing_company:
+          db.delete(existing_company)
+          db.commit()
+          return True
+     return False
---
+++ app/server/routes/routes_cultivator.py
@@ -0,0 +1,40 @@
+from app.server.database.cultivatordb import cultivator_company_helper,getCultivatorData
+from app.server.database.cultivatordb import addCultivatorData,get_id_cultivator_company,update_cultivator_company,delete_company
+from sqlalchemy.orm import Session
+from app.server.database.database import get_db
+from fastapi import APIRouter,Depends
+from app.server.models.cultivatormodel import Cultivator,UpdatedCultivatorSchema,CultivatorSchema
+
+router = APIRouter()
+
+@router.get("/",response_description="Cultivator Data retrieved")
+def get_cultivator_companies(db:Session = Depends(get_db)):
+    cultivator_companies = getCultivatorData(db)
+    if cultivator_companies:
+        return [cultivator_company_helper(fc) for fc in cultivator_companies]
+    
+@router.post("/",response_description="Cultivator Data added successfully")
+def add_farmer_company(data:CultivatorSchema, db:Session = Depends(get_db)):
+    added = addCultivatorData(data.dict(),db)
+    return added     
+
+@router.get("/{id}",response_description="Cultivator retrieved")
+def get_cultivator_company(id:int, db:Session = Depends(get_db)):
+    cultivator_company = get_id_cultivator_company(db,id)
+    if cultivator_company is not None:
+        return cultivator_company_helper(cultivator_company)
+    return "empty list returned: The data doesnt exist"
+
+@router.put("/{id}", response_description= "Cultivator Data updated successfully")
+def update_cultivator_company_data(id:int, req: UpdatedCultivatorSchema,db:Session = Depends(get_db)):
+    req_data = {k:v for k, v in req.dict().items() if v is not None}
+    if update_cultivator_company(db,id,req_data):
+        updated_cultivator_company = get_id_cultivator_company(db,id)
+        return cultivator_company_helper(updated_cultivator_company)     
+    
+@router.delete("/{id}",response_description="Cultivator Data deleted successfully")
+def delete_cultivator_company(id:int, db:Session = Depends(get_db)):
+    if delete_company(id,db):
+        return f"Data with id {id} deleted sucessfully"
+    else:
+        return "Unable to delete cultivator data"
