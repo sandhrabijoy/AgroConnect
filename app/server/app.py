4916305a034e50f4404f2e8069093248f3cb79cc
sandhra <sandhrasonabijoy@gmail.com>
2025-01-17 16:34:35 +0900

    added the connection in app.py for cultivator and cultivator backend is completed

--- app/server/app.py
+++ app/server/app.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.server.routes.fcroutes import router as FCRouter
 from app.server.database.database import Base,engine
-
+from app.server.routes.routes_cultivator import router as CultivatorRouter
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(FCRouter,tags=["farmercompany"],prefix="/farmer")
-
+app.include_router(CultivatorRouter,tags=["cultivator"],prefix="/cultivator")
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
