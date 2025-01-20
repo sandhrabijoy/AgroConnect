1ca3237e72fdea6e91d1139747d5ebd6903f8260
sandhra <sandhrasonabijoy@gmail.com>
2025-01-20 15:05:30 +0900

    changed the images, updated the app.css and image.css to fit the images

--- frontend/agroconnect/src/App.jsx
+++ frontend/agroconnect/src/App.jsx
@@ -6,7 +6,7 @@ import Heading from './atoms/Heading/Heading';
 import Dropdown from './atoms/dropdown/Dropdown';
 import SmallHead from './atoms/SmallHead/SmallHead'
 import Button from './atoms/button/Button';
-import Descr from './atoms/descr/Descr'
+// import Descr from './atoms/descr/Descr'
 import Image from './atoms/Images/Image'
 import Navigation from './atoms/Navigation/Navigation'
 import BasicTable from'./atoms/Table/Table'
@@ -26,7 +26,7 @@ function App() {
           <div className="App">
           <Navigation />
           <Image/>
-          <Descr/>
+          {/* <Descr/> */}
           <Heading className="big-heading"></Heading>
           <SmallHead className= "b-heading"/>         
           <div className='bar'>
--- frontend/agroconnect/src/atoms/Images/Image.jsx
+++ frontend/agroconnect/src/atoms/Images/Image.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import "./Image.css"
-import leftimage from "../../images/leftimage.svg"
-import rightimage from "../../images/rightimage.svg"
+import React from 'react';
+import './Image.css';
+import leftimage from '../../images/leftimage.jpg';
+import rightimage from '../../images/rightimage.jpg';
+import Descr from '../descr/Descr';
+
 const Image = (props) => {
   return (
     <div className={props.className}>
-        <div className={props.className}>
-        <img src={leftimage} alt ="left image"/>
-        <img src={rightimage} alt ="right image"/>
-        </div>
+      <img src={leftimage} alt="left image" className="left-image" />
+      <Descr />
+      <img src={rightimage} alt="right image" className="right-image" />
     </div>
-  )
-}
-export default Image
+  );
+};
+
+export default Image;
