fe95f4134e814bfe42ccd6a171494a3f594340d6
sandhra <sandhrasonabijoy@gmail.com>
2025-01-20 09:49:20 +0900

    final button created using css

--- frontend/agroconnect/src/atoms/button/button.jsx
+++ frontend/agroconnect/src/atoms/button/button.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./button.css"
+
 const Button = () => {
   return (
     <div>
       <button>SUBMIT</button>
+      <button className='button-style'>SUBMIT</button>
     </div>
   )
 }
-export default Button
