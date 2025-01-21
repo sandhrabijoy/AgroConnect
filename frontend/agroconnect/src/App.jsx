abc3e270b3cfa7ab0c9ed2307dc608fee0ecac93
sandhra <sandhrasonabijoy@gmail.com>
2025-01-21 09:09:39 +0900

    installation of axios,import of useState and useEffect

--- frontend/agroconnect/src/App.jsx
+++ frontend/agroconnect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React,{ useState,useEffect } from 'react';
 import './App.css';
 import Heading from './atoms/Heading/Heading';
 import Dropdown from './atoms/dropdown/Dropdown';
@@ -8,8 +8,23 @@ import Image from './atoms/Images/Image'
 import Navigation from './atoms/Navigation/Navigation'
 import BasicTable from'./atoms/Table/Table'
 import BottomNav from './atoms/BottomNav/BottomNav'
+import axios from "axios";
+
 
 function App() {
+  const [lotOptions,setLotOptions] = useState([]);
+  useEffect(()=>{
+    axios
+    .get("http://localhost:8000/farmercompany/")
+    .then((response)=>{
+      console.log(response.data.data);
+      setLotOptions(response.data.data)
+    })
+  })
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log("ButtonClicked");
+  }
 
 
   return (
@@ -22,7 +37,7 @@ function App() {
           <SmallHead className= "b-heading"/>         
           <div className='bar'>
             <Dropdown/>
-            <Button  className="button-style" text="SUBMIT"/></div>
+            <Button  className="button-style" text="SUBMIT" handleChange={handleSubmit}/></div>
           <div className='table-size'><center>
           <BasicTable/>
           </center>
